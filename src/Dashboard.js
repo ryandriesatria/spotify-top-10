8a1a7699533394a7380d0b39ace7056c349011e0
Ryandrie Satria <mryandriesatriad@gmail.com>
2022-10-29 14:32:11 +0700

    add trademark and download timer

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -10,6 +10,9 @@ import * as htmlToImage from "html-to-image";
 import { toPng, toJpeg, toBlob, toPixelData, toSvg } from "html-to-image";
 import "./Dashboard.css";
 import ResponsiveNavbar from "./Navbar";
+import ReactLoading from "react-loading";
+import { FadeIn } from "react-slide-fade-in";
+import Countdown from "react-countdown";
 
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.REACT_APP_CLIENT_ID,
@@ -17,17 +20,18 @@ const spotifyApi = new SpotifyWebApi({
 
 export default function Dashboard({ code }) {
   const accessToken = code;
-  const [search, setSearch] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
+  // const [search, setSearch] = useState("");
+  // const [searchResults, setSearchResults] = useState([]);
   const [playingTrack, setPlayingTrack] = useState();
-  const [lyrics, setLyrics] = useState("");
+  // const [lyrics, setLyrics] = useState("");
   const [topTracks, setTopTracks] = useState();
+  const [loading, setLoading] = useState(true);
 
   function chooseTrack(track) {
     console.log(track);
     setPlayingTrack(track);
-    setSearch("");
-    setLyrics("");
+    // setSearch("");
+    // setLyrics("");
     const spotifyEmbedWindow = document.querySelector("iframe");
     spotifyEmbedWindow.onload = () => {
       spotifyEmbedWindow.contentWindow.postMessage({ command: "toggle" }, "*");
@@ -37,6 +41,8 @@ export default function Dashboard({ code }) {
   function saveToPng() {
     let list = document.getElementById("top-track-list");
     list.style.overflowY = "";
+    let trademark = document.getElementById("trademark");
+    trademark.hidden = false;
     let node = document.getElementById("save-to-jpg");
 
     let viewport = document.querySelector("meta[name=viewport]");
@@ -58,7 +64,7 @@ export default function Dashboard({ code }) {
         quality: 0.95,
         // canvasWidth: "1750",
         // width: "500",
-        height: "1000",
+        height: "1100",
         filter: filter,
       })
       .then(function (dataUrl) {
@@ -71,27 +77,73 @@ export default function Dashboard({ code }) {
     setTimeout(() => {
       list.style.overflowY = "auto";
       viewport.setAttribute("content", "width=device-width, initial-scale=1");
+      trademark.hidden = true;
     }, "500");
   }
+
+  // Renderer callback with condition
+  const renderer = ({ hours, minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a complete state
+      return (
+        <div
+          className='d-flex flex-row justify-content-center align-items-center btn-sm btn-primary my-3 btn-download'
+          onClick={saveToPng}
+          style={{
+            cursor: "pointer",
+            borderRadius: "1000px",
+            // width: "120px",
+          }}
+        >
+          <div className='mx-2'>
+            <i className='fa-solid fa-download mr-2'></i>Save as Jpg
+          </div>
+        </div>
+      );
+    } else {
+      // Render a countdown
+      return (
+        <div
+          className='d-flex flex-row justify-content-center align-items-center btn-sm btn-secondary my-3 '
+          style={{
+            cursor: "pointer",
+            borderRadius: "1000px",
+            // width: "120px",
+            color: "white",
+          }}
+        >
+          <div className='mx-2'>
+            <i className='fa-solid fa-download mr-2'></i>Please wait for{" "}
+            {seconds} seconds
+          </div>
+        </div>
+      );
+    }
+  };
 
   useEffect(() => {
     if (!accessToken) return;
     spotifyApi.setAccessToken(accessToken);
 
-    axios
-      .get("https://api.spotify.com/v1/me/top/tracks", {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-        },
-        params: { limit: 10, offset: 0 },
-      })
-      .then((res) => {
-        setTopTracks(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    setLoading(true);
+    setTimeout(() => {
+      axios
+        .get("https://api.spotify.com/v1/me/top/tracks", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+          },
+          params: { limit: 10, offset: 0 },
+        })
+        .then((res) => {
+          setTopTracks(res.data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+    }, "1500");
   }, [accessToken]);
 
   return (
@@ -102,7 +154,7 @@ export default function Dashboard({ code }) {
           id='dashboard-container'
           className='d-flex flex-column py-2 dashboard-container'
           style={{
-            height: "calc(93vh + 1px)",
+            height: "calc(93vh + 1.5px)",
             width: "auto",
             maxWidth: "500px",
             // marginTop: "8vh",
@@ -141,37 +193,63 @@ export default function Dashboard({ code }) {
             className='flex-grow-1 mt-2 style-7'
             style={{ overflowY: "auto" }}
           >
-            {topTracks
-              ? topTracks.items.map((track, index) => (
+            {loading ? (
+              <div
+                className='d-flex flex-column align-items-center justify-content-center'
+                style={{ height: "400px" }}
+              >
+                <ReactLoading type='spin' />
+                <p className='f4 text-white my-4'>Loading ...</p>
+              </div>
+            ) : (
+              topTracks.items.map((track, index) => (
+                <FadeIn
+                  from={index % 2 === 0 ? "left" : "right"}
+                  positionOffset={0}
+                  triggerOffset={1000}
+                  delayInMilliseconds={index * 400}
+                >
                   <TopTrackList
                     track={track}
                     key={track.id}
                     idx={index}
                     chooseTrack={chooseTrack}
                   />
-                ))
-              : ""}
+                </FadeIn>
+              ))
+            )}
           </div>
           <div
             className='d-flex flex-column justify-content-center align-items-center spotify-player'
             id='spotify-player'
           >
-            <div
-              className='d-flex flex-row justify-content-center align-items-center btn-sm btn-primary mt-2 mb-2 btn-download'
-              onClick={saveToPng}
-              style={{
-                cursor: "pointer",
-                borderRadius: "1000px",
-                width: "120px",
-              }}
-            >
-              <i className='fa-solid fa-download mr-2'></i>Save as Jpg
+            <div hidden id='trademark'>
+              <div
+                className='d-flex flex-row justify-content-center align-items-center btn-sm btn-secondary my-3'
+                style={{
+                  cursor: "pointer",
+                  borderRadius: "1000px",
+                }}
+              >
+                <div className='mx-2'>
+                  <i className='fa-brands fa-spotify mr-2'></i>
+                  Made by @ryandrie with Spotify API
+                </div>
+              </div>
             </div>
-            <Player
-              accessToken={accessToken}
-              trackUri={playingTrack?.uri}
-              defaultTrack={"spotify:track:4jzNb4SziJCRL7K7dVimn7"}
-            />
+            <Countdown date={Date.now() + 5000} renderer={renderer} />
+            <FadeIn
+              from='top'
+              positionOffset={100}
+              triggerOffset={100}
+              delayInMilliseconds={500}
+            >
+              <Player
+                accessToken={accessToken}
+                trackUri={playingTrack?.uri}
+                defaultTrack={"spotify:track:4jzNb4SziJCRL7K7dVimn7"}
+              />
+            </FadeIn>
           </div>
         </Container>
       </div>
